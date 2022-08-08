983e33318b8139ddc2912b126a9f20ff46a4ba0f
Eitan <eitanleviberger@gmail.com>
2022-08-08 18:35:29 +0300

    mock done

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,7 +3,14 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders posts if request succeeds", async () => {
+    // To replace real requests with dummy function
+    window.fetch = jest.fn();
+    // Faking what will be the output of fetch
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
+
     // If fetch worked we'll have li items
     // .getByRole will fail if we have more than 1 item
     // .getAllByRole returns an Array. But instantly, it will not wait
@@ -12,3 +19,5 @@ describe("Async component", () => {
     expect(listItemElements).not.toHaveLength(0);
   });
 });
+
+// Mocks.
