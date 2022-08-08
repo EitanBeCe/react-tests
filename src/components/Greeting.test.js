f2089dacae3de78bb5a1cd5e3f063dd52e208143
Eitan <eitanleviberger@gmail.com>
2022-08-08 17:24:51 +0300

    4 unit tests written

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,11 +1,45 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 // Testing suite
 describe("Greeting component", () => {
-  test("renders Hello World as a text", () => {
+  test("renders 'Hello World' as a text", () => {
     render(<Greeting />);
     const helloWorldElement = screen.getByText("Hello World", { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders 'good to see you' if the button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act - do something (push the button)
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert - following text have to be in the document
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("doesn't render 'good to see you' if the button WAS clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Didn't use .getByText because it will not find it, throw error and test fails, but I exactly want that "good to see you" will not be on the screen
+    // But .queryByText returns null if text is not found
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
